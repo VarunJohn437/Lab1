f84774a0134dc67ce63d0309955cbc3083c858be
Varun John <vjohn8468@conestogac.on.ca>
2024-01-10 15:00:54 -0500

    Initial Commit

---
+++ Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab1
+//
+//  Created by user235215 on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
